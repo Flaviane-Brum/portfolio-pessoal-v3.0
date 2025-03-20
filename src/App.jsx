7bf3c16894d0c06f093786f850a5051748b44ed5
flaviane-brum <flaviane.santos@webglobal.com.br>
2025-03-20 14:45:40 -0300

    update portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import { NotFound } from "./components/NotFound";
-
 import { Loading } from "./components/helpers/Loading";
 const Home = lazy(() => import("./pages/Home"));
 const Project = lazy(() => import("./pages/Project"));
@@ -13,7 +12,10 @@ function App() {
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/project/:id" element={<Project />} />
-					<Route path="*" element={<NotFound text="Página não encontrada." />} />
+					<Route
+						path="*"
+						element={<NotFound text="Página não encontrada." />}
+					/>
 				</Routes>
 			</Suspense>
 		</BrowserRouter>
